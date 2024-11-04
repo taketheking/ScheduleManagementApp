174e813da02440e085755195053e7a299131d720
taketheking <so4524@gmail.com>
2024-11-04 14:44:25 +0900

    init SpringBoot

---
+++ src/main/java/com/example/ScheduleManagementApp/ScheduleManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ScheduleManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ScheduleManagementApp/ScheduleManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ScheduleManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
